f5d1e869bc563eb81d63c0a4c36d661b7613fe88
Kaique Nocetti <kaique.nocetti@medportal.com>
2025-04-22 22:36:53 -0300

    Update socket connection URL to production backend

--- src/services/game-api-service.js
+++ src/services/game-api-service.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://hero-tatics-game-backend-production.up.railway.app');
 
 socket.on('connect', () => {
   console.log('Socket conectado:', socket.id);
